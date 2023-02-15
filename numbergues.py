9077403d8c4a8484bc41526fb0d22c1a17244bda
25Braeden <125329807+25Braeden@users.noreply.github.com>
2023-02-15 12:01:36 -0500

    Add files via upload

---
+++ numbergues.py
@@ -0,0 +1,22 @@
+# number guesser
+
+import random
+n = random.randint(1, 21)
+guess = int(input("Enter any number: "))
+i=0
+while i < 4:
+    i+=1
+    if guess < n:
+        print("Too low")
+        guess = int(input("Enter number again: "))
+    elif guess > n:
+        print("Too high!")
+        guess = int(input("Enter number again: "))
+    else:
+        print("Got it!")
+        break
+
+if guess != n:
+    print("lose")
+    print("The number was", n)
+
